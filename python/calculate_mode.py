1dd99ed09efe790151f5f379a1a9e06c5326059d
fravila08 <105952966+fravila08@users.noreply.github.com>
2022-05-30 12:33:11 -0700

    python complete

--- python/calculate_mode.py
+++ python/calculate_mode.py
@@ -1,2 +1,28 @@
-def calculate_mode():
-    pass
+from operator import countOf
+
+
+def calculate_mode(x):
+    answer=[]
+    compare=0
+    myObj=[]
+    #print(countOf(x, 4.5))
+    for i in x:
+        count=countOf(x, i)
+        myObj.append([i, count])
+    #print(myObj)
+    for i in myObj:
+        if(compare<=i[1]):
+            compare= i[1]
+        else:
+            continue
+    #print(compare)
+    for j in myObj:
+        if(j[1]== compare):
+            answer.append(j[0])
+        else:
+            continue
+    for k in answer:
+        if countOf(answer, k)>1:
+            answer.remove(k)
+    return answer
+#print(calculate_mode(["who", "what", "where", "who"]))
